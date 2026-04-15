0d2efca992a4605c9b31367fc06279ba2cad34cd
julioau15 <julioaugustov@gmail.com>
2026-04-15 11:29:29 -0300

    inicia CRUD de filmes

---
+++ aula-06/model/DAO/filme/filme.js
@@ -0,0 +1,74 @@
+/***********************************************************************************
+ * Objetivo: Arquivo responsável pelo CRUD bo Banco de dados MYSQL na tabela filme
+ * Data: 15/04/2026
+ * Autor: Julio Augusto
+ * Versão: 1.0
+ * *********************************************************************************/
+
+// Import da biblioteca para gerenciar o DB MySQL no node.js
+const knex = require('knex')
+
+// Import do arquivo de configuração para conexão com o DB MySQL
+const knexConfig = require('../../database_config_knex/knexFile')
+
+// Cria a conexão com o DB MySQL
+const knexConex = knex(knexConfig.development)
+
+// Função para inserir dados na tabela de filme
+const insertFilme = async (filme) => {
+    let sql = `
+                INSERT INTO tbl_filme (
+                    nome,
+                    data_lancamento,
+                    duracao,
+                    sinopse,
+                    avaliacao,
+                    valor,
+                    capa
+                ) 
+                VALUES (
+                    '${filme.nome}',
+                    '${filme.data_lancamento}',
+                    '${filme.duracao}',
+                    '${filme.sinopse}',
+                    '${filme.avaliacao}',
+                    '${filme.valor}',
+                    '${filme.capa}'
+                );`
+
+    // Executa script SQL no banco de dados            
+    let result = await knexConex.raw(sql)
+
+    if(result)
+        return true
+    else
+        return false
+}
+
+// Função para atualizar dados da tabela de filme
+const updateFilme = async (filme) => {
+
+}
+
+// Função para retornar todos os dados da tabela de filme
+const selectAllFilme = async () => {
+    
+}
+
+// Função para retornar os dados do filme filtrando pelo id
+const selectByIdFilme = async (id) => {
+    
+}
+
+// Função para deletar um filme pelo id
+const deleteFilme = async (id) => {
+    
+}
+
+module.exports = {
+    insertFilme,
+    updateFilme,
+    selectAllFilme,
+    selectByIdFilme,
+    deleteFilme
+}
---
+++ aula-06/model/database/scriptSQL_filmes.sql
@@ -0,0 +1,37 @@
+# Cria database do projeto de filmes
+CREATE DATABASE IF NOT EXISTS db_filmes_20261_a;
+
+# Ativa o database de filmes
+USE db_filmes_20261_a;
+
+# cria a Table de filmes
+CREATE TABLE IF NOT EXISTS tbl_filme (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(80) NOT NULL,
+    data_lancamento DATE NOT NULL,
+    duracao TIME NOT NULL,
+    sinopse TEXT NOT NULL,
+    avaliacao DECIMAL(3,2) DEFAULT NULL,
+    valor DECIMAL(5,2) NOT NULL DEFAULT 0,
+    capa VARCHAR(255)
+);
+
+SELECT * FROM tbl_filme;
+
+INSERT INTO tbl_filme (
+	nome,
+	data_lancamento,
+	duracao,
+    sinopse,
+	avaliacao,
+    valor,
+	capa
+    ) 
+VALUES (
+	'O Poderoso Chefão',
+	'1972-03-04',
+    '02:55:00',
+	'Don Vito Corleone (Marlon Brando) é o chefe de uma "família" de Nova York que está feliz, pois Connie (Talia Shire), sua filha, se casou com Carlo (Gianni Russo). Porém, durante a festa, Bonasera (Salvatore Corsitto) é visto no escritório de Don Corleone pedindo "justiça", vingança na verdade contra membros de uma quadrilha, que espancaram barbaramente sua filha por ela ter se recusado a fazer sexo para preservar a honra. Vito discute, mas os argumentos de Bonasera o sensibilizam e ele promete que os homens, que maltrataram a filha de Bonasera não serão mortos, pois ela também não foi, mas serão severamente castigados. Vito porém deixa claro que ele pode chamar Bonasera algum dia para devolver o "favor". Do lado de fora, no meio da festa, está o terceiro filho de Vito, Michael (Al Pacino), um capitão da marinha muito decorado que há pouco voltou da 2ª Guerra Mundial. Universitário educado, sensível e perceptivo, ele quase não é notado pela maioria dos presentes, com exceção de uma namorada da faculdade, Kay Adams (Diane Keaton), que não tem descendência italiana mas que ele ama. Em contrapartida há alguém que é bem notado, Johnny Fontane (Al Martino), um cantor de baladas românticas que provoca gritos entre as jovens que beiram a histeria. Don Corleone já o tinha ajudado, quando Johnny ainda estava em começo de carreira e estava preso por um contrato com o líder de uma grande banda, mas a carreira de Johnny deslanchou e ele queria fazer uma carreira solo. Por ser seu padrinho Vito foi procurar o líder da banda e ofereceu 10 mil dólares para deixar Johnny sair, mas teve o pedido recusado. Assim, no dia seguinte Vito voltou acompanhado por Luca Brasi (Lenny Montana), um capanga, e após uma hora ele assinou a liberação por apenas mil dólares, mas havia um detalhe: nas "negociações" Luca colocou uma arma na cabeça do líder da banda. Agora, no meio da alegria da festa, Johnny quer falar algo sério com Vito, pois precisa conseguir o principal papel em um filme para levantar sua carreira, mas o chefe do estúdio, Jack Woltz (John Marley), nem pensa em contratá-lo. Nervoso, Johnny começa a chorar e Vito, irritado, o esbofeteia, mas promete que ele conseguirá o almejado papel. Enquanto a festa continua acontecendo, Don Corleone comunica a Tom Hagen (Robert Duvall), seu filho adotivo que atua como conselheiro, que Carlo terá um emprego mas nada muito importante, e que os "negócios" não devem ser discutidos na sua frente. Os verdadeiros problemas começam para Vito quando Sollozzo (Al Lettieri), um gângster que tem apoio de uma família rival, encabeçada por Phillip Tattaglia (Victor Rendina) e seu filho Bruno (Tony Giorgio). Sollozzo, em uma reunião com Vito, Sonny e outros, conta para a família que ele pretende estabelecer um grande esquema de vendas de narcóticos em Nova York, mas exige permissão e proteção política de Vito para agir. Don Corleone odeia esta idéia, pois está satisfeito em operar com jogo, mulheres e proteção, mas isto será apenas a ponta do iceberg de uma mortal luta entre as "famílias".',
+	'5',
+    '73.57',
+    'https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/90/93/20/20120876.jpg');
