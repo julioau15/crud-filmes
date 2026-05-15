f3728f541f52fb1dd8542117079b8d5401814910
julioau15 <julioaugustov@gmail.com>
2026-05-15 10:16:35 -0300

    muda host para endereço web

--- model/database_config_knex/knexFile.js
+++ model/database_config_knex/knexFile.js
@@ -3,7 +3,7 @@ module.exports = {
       // ⚠️ ALTERAÇÃO: Define o cliente como 'mysql2'
       client: 'mysql2',
       connection: {
-        host: 'localhost',
+        host: 'crud-filmes-aum3.onrender.com',
         user: 'root', // Substitua pelo seu usuário
         password: 'bcd127', // Substitua pela sua senha
         database: 'db_filmes_20261_a',
